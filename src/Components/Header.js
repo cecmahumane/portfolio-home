be21f5c4afbf541031eaffefb6ba340893bc5def
Cecil Mahumane <cecmahumane@gmail.com>
2023-01-26 15:58:01 -0500

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <div>
+            <p>Signature Logo</p>
+        </div>
+        <div>
+            <ul>
+                <li>About Me</li>
+                <li>Portfolio</li>
+                <li>Contact</li>
+            </ul>
+            <div>
+                <p>Download CV</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
