c054cac7eb2afad92e6ac20b84be5ae362f795c9
Cecil Mahumane <cecmahumane@gmail.com>
2023-01-27 14:21:46 -0500

    Hero section added to

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -4,11 +4,16 @@ const HeroSection = () => {
   return (
     <div className='photo-hero-text'>
       <div className='photo'>
-        <img src='/images/headshot.jpg' alt=''/>
+        <img src='/images/headshot.jpg' alt='Smiling male'/>
       </div>
       <div className='hero-text-button'>
+        <h2 className='line1'>I was born</h2>
+        <h2 className='line2'>Different...</h2>
+        <p className='hero-quote'>Not saying I’m special or something. But, <br/> 
+          as a self-taught developer in his 30s, what <br/> 
+          I bring to the table is pretty funny.</p>
         <div className='portfolio-button'>
-          <p>Just look at my dab</p>
+          <p>Just look at my portfolio</p>
         </div>
       </div>
     </div>
