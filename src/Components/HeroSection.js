35825f08a0269786c9d6f091499de34d73ab449d
Cecil Mahumane <cecmahumane@gmail.com>
2023-01-27 08:41:31 -0500

    Headshot added

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
+    <div className='photo-hero-text'>
+      <div className='photo'>
+        <img src='/images/headshot.jpg' alt=''/>
+      </div>
+      <div className='hero-text-button'>
+        <div className='portfolio-button'>
+          <p>Just look at my dab</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
