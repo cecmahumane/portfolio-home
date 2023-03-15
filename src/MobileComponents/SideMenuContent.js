4d51a95566c9ea760526981ff47531d4b0dd2d11
Cecil Mahumane <cecmahumane@gmail.com>
2023-03-15 09:19:25 -0400

    Resume link fixed

--- src/MobileComponents/SideMenuContent.js
+++ src/MobileComponents/SideMenuContent.js
@@ -8,7 +8,7 @@ const SideMenuContent = ({closeSideMenu}) => {
             <Link to='#mobile-bio' smooth onClick={() => closeSideMenu()}><li className='list'><img src='/images/Question.png' alt='question mark icon'/>About</li></Link>
             <Link to='#projects' smooth onClick={() => closeSideMenu()}><li className='list'><img src='/images/RocketLaunch.png' alt='rocket ship icon'/>Portfolio</li></Link>
             <li className='list' onClick={() => window.location = 'mailto:cecmahumane@gmail.com'}><img src='/images/Chats.png' alt='chat bubbles icon'/>Contact</li>
-            <a href='/CecilDeveloperResume.pdf' target="_blank" rel="noopener noreferrer"><li className='cv-download'>Download CV</li></a>
+            <a href='/CecilDeveloperResume-Feb1523.pdf' target="_blank" rel="noopener noreferrer"><li className='cv-download'>Download CV</li></a>
         </ul>
     </div>
   )
